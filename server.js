bc5e5b8481e6fe9b9674efaf8766671a79b34b5b
JoaoPedroPizoli <pizoliejesus@gmail.com>
2024-09-21 20:15:12 -0300

    teste colocando engine node no package.json

--- server.js
+++ server.js
@@ -7,24 +7,6 @@ console.log('Aplicação Express carregada.');
 
 const PORT = process.env.PORT || 4040;
 
-process.on('beforeExit', (code) => {
-    console.log('Processo prestes a encerrar com código:', code);
-  });
-  
-  process.on('exit', (code) => {
-    console.log('Processo encerrado com código:', code);
-  });
-  
-  process.on('uncaughtException', (err) => {
-    console.error('Exceção não capturada:', err);
-  });
-  
-  process.on('unhandledRejection', (reason, promise) => {
-    console.error('Rejeição não tratada em:', promise, 'Motivo:', reason);
-  });
-  
-  
-
 app.listen(PORT, () => {
     console.log(`API RODANDO na porta ${PORT}!`);
 });
