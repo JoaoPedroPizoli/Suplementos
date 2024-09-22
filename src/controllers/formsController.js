5813cca5996b3bd53995799df0e01bebd6ce7a56
JoaoPedroPizoli <pizoliejesus@gmail.com>
2024-09-22 02:28:00 -0300

    logs no forms

--- src/controllers/formsController.js
+++ src/controllers/formsController.js
@@ -1,4 +1,5 @@
-const Forms = require('../models/forms')
+// Exemplo no formsController.js
+const Forms = require('../models/forms');
 
 class FormController {
   async create(req, res) {
@@ -6,7 +7,14 @@ class FormController {
     console.log("Request Body: ", req.body);
 
     try {
+      console.log('Iniciando criação de um novo formulário...');
+      const startTime = Date.now();
+      
       await Forms.new(nome, email, whatsapp);
+      
+      console.log('Formulário criado com sucesso!');
+      console.log(`Tempo de execução: ${Date.now() - startTime} ms`);
+      
       res.status(200).json({ success: true, message: 'Dados obtidos com sucesso!' });
     } catch (error) {
       console.error("Erro ao criar formulário: ", error);
