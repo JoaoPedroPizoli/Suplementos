97f6b6f82f196ffb75f4af9d19f26f0b8cd62ccb
JoaoPedroPizoli <pizoliejesus@gmail.com>
2024-09-22 02:54:44 -0300

    a

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -4,9 +4,7 @@ const formController = require('../controllers/formsController');
 
 
 router.post('/', formController.create);
-router.get('/teste', (req, res) => {
-    res.send('Bem-vindo à API!');
-  });
+
   
 
 module.exports = router;
