d60a0c499d87f6f064f9be5479d467ed4d4e542f
JoaoPedroPizoli <pizoliejesus@gmail.com>
2024-09-22 02:58:20 -0300

    a

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
-require('dotenv').config();
-const formRouter = require('./routers/routers'); 
-const cors = require('cors');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+// Configuração do CORS para permitir apenas o seu domínio na Vercel
 app.use(cors({
-  origin: 'https://frontend-suplementos.vercel.app/', 
+  origin: 'https://https://frontend-suplementos.vercel.app/', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials: true 
 }));
+
+// Outros middlewares e rotas
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-app.use('/', formRouter);
+// Definição das rotas da API
+app.use('/', require('./routers/routers'));
 
 module.exports = app;
