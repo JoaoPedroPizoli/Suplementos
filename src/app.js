c36e58e114220b22ae994a35f9855c173604d263
JoaoPedroPizoli <pizoliejesus@gmail.com>
2024-09-22 01:53:13 -0300

    coloca uma / na url do front

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const formRouter = require('./routers/routers');
 
 app.use(cors({
-  origin: 'https://frontend-suplementos.vercel.app', 
+  origin: 'https://frontend-suplementos.vercel.app/', 
 }));
 
 app.use(express.json());
