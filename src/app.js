d92a7bc19cd7af079d04c4b99f22da00bf60753d
JoaoPedroPizoli <pizoliejesus@gmail.com>
2024-09-23 17:23:11 -0300

    tirando um z aleatorio q escrevi :(

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true 
-}));z
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
