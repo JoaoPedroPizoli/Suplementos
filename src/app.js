6079dc8d209dd0f4e45af2af8690ef33da43b40c
JoaoPedroPizoli <pizoliejesus@gmail.com>
2024-09-25 16:14:57 -0300

    altera o cors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const allowedOrigins = ['https://starshape-lancamentos.com.br/'];
 
 app.use(cors({
-  origin: 'https://starshape-lancamentos.com.br', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-  credentials: true 
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
 }));
 
 app.use(express.json());
